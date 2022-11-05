35aa93224cedac20a12447aece668ee3ec9f390d
Artem Bogoslovsky <74101177+Metra345@users.noreply.github.com>
2022-11-05 15:52:38 +0300

    Refactoring due some optimization

--- Program.cs
+++ Program.cs
@@ -5,50 +5,43 @@ namespace Pars
 {
     internal class Program
     {
-        const string resultFile = "ololog.txt";
-        const string regexMask = @"threw an exception during launch";
-        const string fileRegex = @".*\.log";
+        static string resultFile = "ololog.txt";
+        static string regexMask = @"threw an exception during launch";
+        static string fileRegex = @".*\.log";
         static void Main(string[] args)
         {
             if (args.Length == 0) return;
 
             if (!File.Exists(resultFile)) File.Create(resultFile);
+
             File.WriteAllText(resultFile, "");
 
             List<string> files = new List<string>();
 
-            if (File.Exists(args[0]))
-            {
-                files.Add(args[0]);
-            }
-            if (Directory.Exists(args[0]))
-            {
-                files.AddRange(FileNamesFromDirectory(args[0]));
-            }
-            foreach (string file in files)
-            {
-                ProcessSingleFile(file);
-            }
+            if (File.Exists(args[0]) && new Regex(fileRegex).IsMatch(args[0])) files.Add(args[0]);
+
+            if (Directory.Exists(args[0])) files.AddRange(FileNamesFromDirectory(args[0]));
+
+            //if (files.Count == 0) return;
+
+            foreach (string file in files) ProcessSingleFile(file, new Regex(regexMask));
         }
         public static List<string> FileNamesFromDirectory(string dir)
         {
             return Directory
                         .EnumerateFiles(dir)
-                        .Where(x => new Regex(fileRegex)//коряво(лишний энтер) но красиво
-                        .IsMatch(x))
+                        .Where(x => new Regex(fileRegex).IsMatch(x))
                         .ToList();
         }
 
-        public static void ProcessSingleFile(string fileName)
+        public static void ProcessSingleFile(string fileName, Regex regex)
         {
             using (StreamWriter sw = File.CreateText(resultFile))
             {
                 foreach (string processedLine in File
                 .ReadAllLines(fileName)
-                .Select((line, i) => (String//коряво(лишний энтер) но красиво
-                .Format("[Файл {0}][Строка {1}] ", fileName, i), line))
-                .Where(prefixAndLine => new Regex(regexMask)//коряво(лишний энтер) но красиво
-                .IsMatch(prefixAndLine.line))
+                .Select((line, i) => (String.Format("[Файл {0}][Строка {1}] ", fileName, i), line))
+                .Where(prefixAndLine => regex.IsMatch(prefixAndLine.line))
                 .Select(prefixAndLine => prefixAndLine.Item1 + prefixAndLine.line)
                 .ToArray())
                 {
